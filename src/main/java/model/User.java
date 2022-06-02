3ef4139ccac7603efdbe395d645c46888c705609
chuong0608 <chuongtep113@gmail.com>
2022-06-02 12:02:11 +0700

    done model user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,72 @@
 package model;
 
-public class User {
+import java.time.LocalDate;
 
+public class User {
+    private int id;
+    private String fullname;
+    private String email;
+    private String avatar;
+    private LocalDate dateOfBirth;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String fullname, String email, String avatar, LocalDate dateOfBirth, String password) {
+        this.id = id;
+        this.fullname = fullname;
+        this.email = email;
+        this.avatar = avatar;
+        this.dateOfBirth = dateOfBirth;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
